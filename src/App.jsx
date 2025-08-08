fd1cee46f672e490f7c0a2276aa0f3f380aa7eec
FerasGIT <ferasny@gmail.com>
2025-08-08 20:06:33 +0300

    now login is persistent

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,21 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './Pages/Home'
 import Login from './Pages/Login'
-import { useState } from 'react'
+import checkSession from './services/checkSession'
+import { useEffect, useState } from 'react'
 function App() {
   const [user, setUser] = useState(null)
+  const [token, setToken] = useState(null)
+  const checkToken = async () => {
+    let currentAdmin = await checkSession()
+    const token = localStorage.getItem('token')
+    if (token) {
+      setUser(currentAdmin)
+    }
+  }
+  useEffect(() => {
+    checkToken()
+  }, [])
   return (
     <>
       <Routes>
