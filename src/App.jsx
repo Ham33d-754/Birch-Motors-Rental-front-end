595ae09ccb7895cfd88ee3341d5da8eb7738bdd8
Ham33d-754 <mahmoodish873@gmail.com>
2025-08-10 09:34:55 +0300

    merging

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './Pages/Home'
 import Login from './Pages/Login'
+import CarDetails from './Pages/CarDetails'
 import { useState } from 'react'
 function App() {
   const [user, setUser] = useState(null)
@@ -9,8 +10,8 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-
         <Route path="/signIn" element={<Login setUser={setUser} />} />
+        <Route path='/car' element={< CarDetails />}/>
       </Routes>
     </>
   )
