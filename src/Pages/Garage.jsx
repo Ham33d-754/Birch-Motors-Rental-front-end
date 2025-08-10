f89e84af6247e4cee960a56aca4529fba39590ef
10Fawaz <faaljar2@gmail.com>
2025-08-10 14:20:30 +0300

    removed garagecard component and transferredn it on Garage.jsx

--- src/Pages/Garage.jsx
+++ src/Pages/Garage.jsx
@@ -1,9 +1,26 @@
 import { Link, Routes, Route } from 'react-router-dom'
 import CreateGarage from '../Pages/CreateGarage'
-import GarageCard from '../Components/GarageCard'
 import GarageDetails from '../Pages/GarageDetails'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Client, { BASE_URL } from '../services/api'
 
 const Garage = () => {
+  const [garages, setGarages] = useState([])
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const linkGarage = async () => {
+      const res = await Client.get(`${BASE_URL}/garages/`)
+      setGarages(res.data.garages)
+    }
+    linkGarage()
+  }, [])
+
+  const handleClick = (id) => {
+    navigate(`${id}`)
+  }
+
   return (
     <>
       <h2>Garage</h2>
@@ -11,9 +28,23 @@ const Garage = () => {
         <Link to="/garages/create">Create Garage</Link>
       </nav>
 
+      <>
+        <h3>View Garages</h3>
+        {garages.length === 0 ? (
+          <p>No Garages Available</p>
+        ) : (
+          <ul>
+            {garages.map((garage) => (
+              <li key={garage._id} onClick={() => handleClick(garage._id)}>
+                <h3>{garage.name} Garage </h3>
+              </li>
+            ))}
+          </ul>
+        )}
+      </>
+
       <Routes>
         <Route path="create" element={<CreateGarage />} />
-        <Route path="/" element={<GarageCard />} />
         <Route path="/:id" element={<GarageDetails />} />
       </Routes>
     </>
