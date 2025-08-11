4b617a13cae2b2464a3ef9111a075077e27e525c
10Fawaz <faaljar2@gmail.com>
2025-08-11 11:06:04 +0300

    .

--- src/Pages/Booking.jsx
+++ src/Pages/Booking.jsx
@@ -1,17 +1,8 @@
-import { useEffect, useState } from 'react'
-import Client, { BASE_URL } from '../services/api'
-
-const Booking = ({ user }) => {
-  const [cars, setCars] = useState([])
-  const [selectedCar, setSelectedCar] = useState(null)
-
-  useEffect(() => {
-    const linkCars = async () => {
-      const res = await Client.get(`${BASE_URL}/cars`)
-      setCars(res.data.cars)
-    }
-    linkCars()
-  }, [])
-  return <></>
+const Booking = () => {
+  return (
+    <>
+    
+    </>
+  )
 }
 export default Booking
