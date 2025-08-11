a0579d5b79d9403c162745cd68a2b43eb5591455
FerasGIT <ferasny@gmail.com>
2025-08-12 00:15:02 +0300

    fixed styling on pop up forms

--- src/Components/Createcar.jsx
+++ src/Components/Createcar.jsx
@@ -36,8 +36,8 @@ const CreateCar = ({ garageId, isOpen, hadelOpen, fetchGarageAndCars }) => {
 
   return (
     <>
-      <div style={styles.overlay}>
-        <div style={styles.modal}>
+      <div className="overlay">
+        <div className="modal">
           {msg}
           <h2>Add a new Car</h2>
           <form onSubmit={handleSubmit}>
@@ -95,23 +95,5 @@ const CreateCar = ({ garageId, isOpen, hadelOpen, fetchGarageAndCars }) => {
     </>
   )
 }
-const styles = {
-  overlay: {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  modal: {
-    background: 'white',
-    padding: '20px',
-    borderRadius: '8px',
-    boxShadow: '0 2px 10px rgba(0,0,0,0.1)'
-  }
-}
+
 export default CreateCar
