f63c1e6eae459b870b421d4c2e4dcb4d96566ef6
10Fawaz <faaljar2@gmail.com>
2025-08-11 10:25:59 +0300

    made the useEffect and linking in booking.jsx

--- src/Pages/Booking.jsx
+++ src/Pages/Booking.jsx
@@ -1,8 +1,17 @@
-const Booking = () => {
-  return (
-    <>
-    
-    </>
-  )
+import { useEffect, useState } from 'react'
+import Client, { BASE_URL } from '../services/api'
+
+const Booking = ({ user }) => {
+  const [cars, setCars] = useState([])
+  const [selectedCar, setSelectedCar] = useState(null)
+
+  useEffect(() => {
+    const linkCars = async () => {
+      const res = await Client.get(`${BASE_URL}/cars`)
+      setCars(res.data.cars)
+    }
+    linkCars()
+  }, [])
+  return <></>
 }
 export default Booking
