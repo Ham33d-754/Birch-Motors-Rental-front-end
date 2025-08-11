bf7ebd4e7c18da94276581ed15e97382e76fc102
10Fawaz <faaljar2@gmail.com>
2025-08-11 11:25:18 +0300

    linked booking with backend

--- src/Pages/Booking.jsx
+++ src/Pages/Booking.jsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react"
+import Client, { BASE_URL } from "../services/api"
+
 const Booking = () => {
+
+  const [cars, setcars] = useState([])
+  const [selectedCar, setSelectedCar] = useState(null)
+
+  useEffect(() => {
+    const linkCars = async () => {
+      const res = await Client.get(`${BASE_URL}/cars`)
+      setcars(res.data.cars)
+    } 
+    linkCars()
+
+  },[])
   return (
     <>
     
