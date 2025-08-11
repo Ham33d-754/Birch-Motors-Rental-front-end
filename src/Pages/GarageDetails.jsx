3a9ff1a99bc074769ee2031feaa74e6603a5bf6e
10Fawaz <faaljar2@gmail.com>
2025-08-11 13:42:24 +0300

    fixed garagedetails

--- src/Pages/GarageDetails.jsx
+++ src/Pages/GarageDetails.jsx
@@ -14,7 +14,7 @@ const GarageDetails = () => {
     const res = await Client.get(`${BASE_URL}/garages/${garageId}`)
     const res2 = await Client.get(`${BASE_URL}/cars/garage/${garageId}`)
     setGarage(res.data.garage)
-    setListOfCars(res2.data.car)
+    setListOfCars(res2.data.cars)
   }
 
   useEffect(() => {
@@ -27,28 +27,27 @@ const GarageDetails = () => {
   console.log(garage)
   console.log(listOfCars)
 
-  let display =
-    garage && listOfCars ? (
-      <div>
-        <h2>Garage Details</h2>
-        <h2>{garage.name}</h2>
-        <h2>{garage.location}</h2>
-        <button onClick={hadelOpen}>Add new Car</button>
-        <CreateCar
-          garageId={garageId}
-          isOpen={isOpen}
-          hadelOpen={hadelOpen}
-          fetchGarageAndCars={fetchGarageAndCars}
-        />
-        {listOfCars.length ? (
-          listOfCars.map((car) => <CarCard car={car} key={car._id} />)
-        ) : (
-          <h2>no cars avaliable</h2>
-        )}
-      </div>
-    ) : (
-      <h2>Loading...</h2>
-    )
+  let display = garage ? (
+    <div>
+      <h2>Garage Details</h2>
+      <h2>{garage.name}</h2>
+      <h2>{garage.location}</h2>
+      <button onClick={hadelOpen}>Add new Car</button>
+      <CreateCar
+        garageId={garageId}
+        isOpen={isOpen}
+        hadelOpen={hadelOpen}
+        fetchGarageAndCars={fetchGarageAndCars}
+      />
+      {listOfCars ? (
+        listOfCars.map((car) => <CarCard car={car} key={car._id} />)
+      ) : (
+        <h2>no cars avaliable</h2>
+      )}
+    </div>
+  ) : (
+    <h2>Loading...</h2>
+  )
   return <>{display}</>
 }
 export default GarageDetails
