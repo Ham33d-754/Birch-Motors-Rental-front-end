1b9349ab2c794cccd5e6b8ef65d3444466749f42
FerasGIT <ferasny@gmail.com>
2025-08-11 12:50:10 +0300

    new push

--- src/Pages/GarageDetails.jsx
+++ src/Pages/GarageDetails.jsx
@@ -25,26 +25,27 @@ const GarageDetails = () => {
     setIsOpen(!isOpen)
   }
 
-  let display =
-    garage && listOfCars ? (
-      <div>
-        <h2>Garage Details</h2>
-        <h2>{garage.name}</h2>
-        <h2>{garage.location}</h2>
-        <button onClick={hadelOpen}>Add new Car</button>
-        <CreateCar
-          garageId={garageId}
-          isOpen={isOpen}
-          hadelOpen={hadelOpen}
-          fetchGarageAndCars={fetchGarageAndCars}
-        />
-        {listOfCars.map((car) => (
-          <CarCard car={car} key={car._id} />
-        ))}
-      </div>
-    ) : (
-      <h2>Loading...</h2>
-    )
+  let display = garage ? (
+    <div>
+      <h2>Garage Details</h2>
+      <h2>{garage.name}</h2>
+      <h2>{garage.location}</h2>
+      <button onClick={hadelOpen}>Add new Car</button>
+      <CreateCar
+        garageId={garageId}
+        isOpen={isOpen}
+        hadelOpen={hadelOpen}
+        fetchGarageAndCars={fetchGarageAndCars}
+      />
+      {listOfCars ? (
+        listOfCars.map((car) => <CarCard car={car} key={car._id} />)
+      ) : (
+        <h2>no cars avaliable</h2>
+      )}
+    </div>
+  ) : (
+    <h2>Loading...</h2>
+  )
   return <>{display}</>
 }
 export default GarageDetails
