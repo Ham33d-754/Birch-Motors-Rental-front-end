264f6f4b4dc714b41db124e0b749678926b362d1
Ham33d-754 <mahmoodish873@gmail.com>
2025-08-12 08:32:09 +0300

    added a create review component to viewBookings

--- src/Pages/ViewBookings.jsx
+++ src/Pages/ViewBookings.jsx
@@ -1,38 +1,87 @@
-import { useEffect, useState } from "react"
-import Client, { BASE_URL } from "../services/api"
+import CreateReview from './CreateReview'
 
-const ViewBookings = ({user}) => {
+import { useEffect, useState } from 'react'
+import Client, { BASE_URL } from '../services/api'
+
+const ViewBookings = ({ user }) => {
   const [bookings, setBookings] = useState([])
+  //
+  // const [reviews, setReviews] = useState(null)
+  const [selectedCarId, setSelectedCarId] = useState(null)
+
+  const [toggle, setToggle] = useState(false)
+  const linkReview = async () => {
+    const res = await Client.get(`${BASE_URL}/reviews`)
+    console.log(res.data.reviews)
+    setReviews(res.data.reviews)
+  }
+  useEffect(() => {
+    linkReview()
+  }, [])
+  const handleToggle = () => {
+    setToggle(!toggle)
+  }
   useEffect(() => {
     const linkBookings = async () => {
       const res = await Client.get(`${BASE_URL}/bookings/user/${user._id}`)
       setBookings(res.data.bookings)
     }
-
-  },[user])
+  }, [user])
   return (
     <>
-    <h2>My Bookings</h2>
-    {
-      bookings.length === 0 ? (
+      <h2>My Bookings</h2>
+      {bookings.length === 0 ? (
         <p>No Bookings yet</p>
-      ): (
+      ) : (
         <ul>
-          {
-            bookings.map((booking) => (
-              <li key={booking._id}>
-                <p><strong>Car: </strong>{booking.car.name}</p>
-                <p><strong>Type: </strong>{booking.car.carType}</p>
-                <p><strong>Payment: </strong>{booking.payMethod}</p>
-                <p><strong>Booked Duration : </strong>{booking.hours} Hours</p>
-                <img src={booking.car.image}  />
-              </li>
-            ))
-          }
+          {bookings.map((booking) => (
+            <li key={booking._id}>
+              <p>
+                <strong>Car: </strong>
+                {booking.car.name}
+              </p>
+              <p>
+                <strong>Type: </strong>
+                {booking.car.carType}
+              </p>
+              <p>
+                <strong>Payment: </strong>
+                {booking.payMethod}
+              </p>
+              <p>
+                <strong>Booked Duration : </strong>
+                {booking.hours} Hours
+              </p>
+              <img src={booking.car.image} />
+              <h2>create review</h2>
+              <button
+                onClick={() => {
+                  setToggle(true)
+                  setSelectedCarId(booking.car._id)
+                }}
+              >
+                Add Review
+              </button>
+              <CreateReview
+                toggle={toggle}
+                handleToggle={handleToggle}
+                linkReview={linkReview}
+                carId={selectedCarId}
+              />
+            </li>
+          ))}
         </ul>
-      )
-    }
-    
+      )}
+
+      {/* {reviews ? (
+        <div className="reviews">
+          {reviews.map((review) => (
+            <ReviewCard review={review} key={review._id} />
+          ))}
+        </div>
+      ) : (
+        <h2>loading...</h2>
+      )} */}
     </>
   )
 }
